f0e04b8b64e7c8225fca1e397b700da510fe1697
Enzo Rossetto <enzojmrossetto@gmail.com>
2020-05-12 21:02:38 -0300

    Added content to the methods so they can complete the bootcamp challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,80 @@ app.use(cors());
 
 const repositories = [];
 
+function validateUuid(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ message: 'Invalid repository ID.' })
+  }
+
+  return next();
+}
+
+function verifyIfRepositoryExists(request, response, next) {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ message: 'No project with given ID.' });
+  } else {
+    request.params.index = repositoryIndex;
+  }
+
+  return next();
+}
+
+function verifyIfBodyParamsArePresent(request, response, next) {
+  const { title, url, techs } = request.body;
+
+  if (!title || !url || !techs || techs.length === 0) {
+    const message = `Missing information: ${!title ? 'title, ' : ''}${!url ? 'url, ' : ''}${!techs || techs.length === 0 ? 'techs' : ''}.`;
+
+    return response.status(400).json({ message });
+  }
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", verifyIfBodyParamsArePresent, (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateUuid, verifyIfRepositoryExists, (request, response) => {
+  const { index } = request.params;
+  const { title, url, techs } = request.body;
+
+  repositories[index].title = title;
+  repositories[index].url = url;
+  repositories[index].techs = techs;
+
+  return response.json(repositories[index]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateUuid, verifyIfRepositoryExists, (request, response) => {
+  const { index } = request.params;
+
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateUuid, verifyIfRepositoryExists, (request, response) => {
+  const { index } = request.params;
+
+  repositories[index].likes += 1;
+
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
